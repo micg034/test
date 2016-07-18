bb3638c16454b4f44339fae646045e634c632313
micg034 <michelle_go_3@hotmail.com>
2016-07-18 20:26:54 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+var app = angular.module('CV', ['ngRoute']);
+app.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'HomeController',
+    templateUrl: 'template/home.html'
+  })
+  .when('/photos/:id',{
+    controller: 'PhotoController',
+    templateUrl: 'template/photo.html'
+  })
+  .otherwise({
+    redirectTo: '/',
+  });
+});
